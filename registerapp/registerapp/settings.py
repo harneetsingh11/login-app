edb44ddb7b5ee88e094a69096bfcd1586d181f9f
harneetsingh11 <harneet11singh@gmail.com>
2024-03-22 05:50:22 +0530

    final

--- registerapp/registerapp/settings.py
+++ registerapp/registerapp/settings.py
@@ -27,13 +27,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['13.201.187.130', 'localhost']
 
-
-CSRF_TRUSTED_ORIGINS = [
-        'http://nginx-service',
-        'http://localhost',
-        'http://13.201.187.130'
-]
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -50,7 +43,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    #'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
